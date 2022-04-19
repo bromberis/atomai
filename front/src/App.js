07efc73e1b9b93e7a6cee99f050644159c031251
emilija <emilijatyla@gmail.com>
2022-04-19 11:52:27 +0300

    add history table

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,15 @@
 import "./App.css";
 
 import Income from "./components/Income";
+import History from "./components/History";
 
 function App() {
   return (
     <div>
-      {" "}
+      {/* {" "}
       <div className="App">Atomic money app.</div>
-      <Income />
+      <Income /> */}
+      <History />
     </div>
   );
 }
--- front/src/components/HistoryTable.js
+++ front/src/components/HistoryTable.js
@@ -1,13 +1,27 @@
 import React from "react";
+import "./History.css";
 
-function HistoryTable({ name, email, expenses }) {
-  console.log(expenses);
-
+function HistoryTable({ name, email }) {
   return (
     <>
-      <p>{name}</p>
-      <p>{email}</p>
-      <p></p>
+      <tr>
+        <td>data</td>
+        <td>{name}</td>
+        <td>{email}</td>
+        <td></td>
+        <td>
+          <button className="btn btn-outline-warning m-1 custom-button">
+            Update
+          </button>
+          <button className="btn btn-outline-danger m-1 custom-button">
+            Delete
+          </button>
+        </td>
+      </tr>
+
+      {/* <div>{name}</div>
+      <div>{email}</div>
+      <div>{JSON.stringify(expenses)}</div> */}
     </>
   );
 }
