ea5b33cfcb0dde0c75f4e5b00d72677778ad0fc5
emilija <emilijatyla@gmail.com>
2022-05-02 08:39:53 +0300

    stat

--- front/src/api/library/UsersAPI.js
+++ front/src/api/library/UsersAPI.js
@@ -11,13 +11,9 @@ export async function createUserData(data) {
   return response;
 }
 
-// income
+// INCOME
 
 export async function findIncomeDataAndUpdate(data, id, subID) {
-  // console.log(data);
-  // console.log(id);
-  // console.log(subID);
-
   const response = await axiosUser
     .patch(`/${id}/inc/upd/${subID}`, JSON.stringify(data))
     .then((result) => {
@@ -63,12 +59,31 @@ export async function findIncomeAndDelete(id, subID) {
     });
 }
 
-//expenses
+export async function createUserIncome(id, data) {
+  console.log(data);
+  console.log(id);
+  const response = await axiosUser
+    .patch(`/${id}/inc/`, JSON.stringify(data))
+    .then((result) => {
+      console.log("Success:", result);
+      swal({
+        text: "Įrašas išsaugotas!",
+
+        icon: "success",
+        timer: 500,
+      });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      swal("Oops", "Klaida!", "error");
+    });
+
+  return response;
+}
+
+// EXPENSES
 
 export async function findExpensesDataAndUpdate(data, id, subID) {
-  // console.log(data);
-  // console.log(id);
-  // console.log(subID);
   const response = await axiosUser
     .patch(`/${id}/exp/upd/${subID}`, JSON.stringify(data))
     .then((result) => {
@@ -106,31 +121,7 @@ export async function findExpensesAndDelete(id, subID) {
     });
 }
 
-export async function createUserIncome(id, data) {
-  console.log(data);
-  console.log(id);
-  const response = await axiosUser
-    .patch(`/${id}/inc/`, JSON.stringify(data))
-    .then((result) => {
-      console.log("Success:", result);
-      swal({
-        text: "Įrašas išsaugotas!",
-
-        icon: "success",
-        timer: 500,
-      });
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      swal("Oops", "Klaida!", "error");
-    });
-
-  return response;
-}
-
 export async function createUserExpense(id, data) {
-  // console.log(`EXPENSE`);
-
   console.log(id);
   console.log(data);
   const response = await axiosUser
--- front/src/components/statistics/Statistics.jsx
+++ front/src/components/statistics/Statistics.jsx
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getAllUsersData } from "../../api/library/UsersAPI";
 
 function Statistics() {
+  const [income, setIncome] = useState([]);
+
+  useEffect(() => {
+    getIncome();
+  }, []);
+
+  function getIncome() {
+    getAllUsersData().then((res) => {
+      setIncome(res.data.data.users[0].income);
+    });
+  }
+
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth() + 1;
+
+  console.log(income.reduce((n, { sum }) => n + sum, 0));
+
   return <div>statistics</div>;
 }
 
