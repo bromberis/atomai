897da42763078d8f960c2e8fe355e4824d005db0
JusterisJ <justasjusteris@gmail.com>
2022-05-11 20:32:17 +0300

    password enforce 1number 1 uppercase

--- front/src/components/registrationLogin/Registration.jsx
+++ front/src/components/registrationLogin/Registration.jsx
@@ -29,7 +29,7 @@ export default function Registration() {
   let password = watch("password");
 
   return (
-    <div className="Registration-container">
+    <div className="Registration-container ">
       <h1>Registracija</h1>
       <form className="Registration-form" onSubmit={handleSubmit(onSubmit)}>
         <input
@@ -44,6 +44,8 @@ export default function Registration() {
             pattern: /^[[^A-Za-ząčęėįšųūžĄČĘĖĮŠŲŪŽ0-9_]*$/i,
           })}
         />
+        {/* /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/ */}
+        {/* /^[[^A-Za-ząčęėįšųūžĄČĘĖĮŠŲŪŽ0-9_]*$/i */}
         <span className="text-danger fw-light">
           {errors.name?.type === "pattern" && "Negali būti specialų simbolių"}
           {errors.name?.type === "required" && "Vardas būtinas"}
@@ -88,12 +90,15 @@ export default function Registration() {
             required: true,
             minLength: 8,
             maxLength: 20,
+            pattern: /^(?=.*[0-9])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]/,
+            // /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/
           })}
         />
         <span className="text-danger fw-light">
           {errors?.password?.type === "required" && "Slaptažodis būtinas"}
           {errors?.password?.type === "minLength" && "Turi būti bent 8 simboliai"}
           {errors?.password?.type === "maxLength" && "Ne daugiau kaip 20 simbolių"}
+          {errors?.password?.type === "pattern" && "Turi būti bent 1 didžioji raidė ir bent 1 simbolis"}
         </span>
         <input
           className="reg-input"
