56c4dce6ca3f4c4683b625e9fe77d4d5df06dfda
JusterisJ <justasjusteris@gmail.com>
2022-05-17 09:02:20 +0300

    a

--- front/src/components/ProtectedRoutes.jsx
+++ front/src/components/ProtectedRoutes.jsx
@@ -1,30 +1,19 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet, Navigate, useNavigate } from "react-router-dom";
 import { useGlobalUserContext, UserContext } from "./context/UserContext";
 
 export default function ProtectedRoutes(props) {
-  let navigate = useNavigate();
   let { userData } = useGlobalUserContext(UserContext);
-  //if (userData.role === undefined) return "... LOADING ...";
-  console.log(userData.role, props.roleRequired, props);
-  console.log(userData.role === props.roleRequired);
-  // if (userData.role === undefined) {
-  //   setTimeout(() => {
-  //     navigate("/");
-  //   }, 1000);
-  //   return "";
-  // }
-  console.log(userData?.role);
-  function test() {}
 
   if (userData.hasOwnProperty("email")) {
     if (props.roleRequired) {
       return userData.role == props.roleRequired ? <Outlet /> : <Navigate to="/" />;
     } else if (userData.hasOwnProperty("role")) {
-      console.log(userData.role);
       return userData.role ? <Outlet /> : <Navigate to="/" />;
     } else {
-      return <Navigate to="/" />;
+      setTimeout(() => {
+        return <Navigate to="/" />;
+      }, 1000);
     }
   }
 }
